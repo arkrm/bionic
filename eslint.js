380faf9fed80993a614751ac02722ce139b53fc8
vdfor <jiakun.li@qq.com>
2020-05-29 10:25:06 +0800

    eslint rule: jsx-a11y/media-has-caption set off

--- eslint.js
+++ eslint.js
@@ -67,6 +67,7 @@ module.exports = {
     'import/prefer-default-export': ['off'],
     'jsx-a11y/anchor-is-valid': ['off'],
     'jsx-a11y/click-events-have-key-events': ['off'],
+    'jsx-a11y/media-has-caption': ['off'],
     'jsx-a11y/no-noninteractive-element-interactions': ['off'],
     'jsx-a11y/no-static-element-interactions': ['off'],
     'linebreak-style': 'off',
